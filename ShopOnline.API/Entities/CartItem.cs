8ccf0a60be60654a18f8fd249e79a3a726efb8ea
MazzGuille <mazzcode@gmail.com>
2022-05-29 23:53:25 -0300

    Agregar archivos de proyecto.

---
+++ ShopOnline.API/Entities/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopOnline.API.Entities
+{
+    public class CartItem
+    {
+        public int id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+    }
+}
